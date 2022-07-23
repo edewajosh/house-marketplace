c37fea980e7a9e845f43804b4de6f9205b0ae8ee
edewajosh <joshedewa@gmail.com>
2022-07-23 14:11:20 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Explore from './pages/Explore'
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from'./pages/Offers';
+import Profile from "./pages/Profile";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Explore />}/>
+        <Route path="/offers" element={<Offers />}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/forgot-password" element={<ForgotPassword />}/>
+      </Routes>
+      <NavBar />
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtbBIiBtzWRyq5f6rmIfnOiPoDcR_IHFo",
+  authDomain: "house-marketplace-app-4d26b.firebaseapp.com",
+  projectId: "house-marketplace-app-4d26b",
+  storageBucket: "house-marketplace-app-4d26b.appspot.com",
+  messagingSenderId: "642956043088",
+  appId: "1:642956043088:web:34963204981f771f6bc255"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
